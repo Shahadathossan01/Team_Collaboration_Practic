82e4657a7982bbcd1fca6935c36a9bb6547e7220
topu <topu@gmail.com>
2025-08-16 16:35:48 +0600

    Fixed: delete developer2 variable

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const TeamDetails=()=>{
     const developer1='I am sahadat hosen. I hold a B.S.c in computer science and engineering. Now i am a full stack web developer. In this project i work for Backend to create REST API.'
-    const developer2='I am sahadat hosen. I hold a B.S.c in computer science and engineering. Now i am a full stack web developer. In this project i work for Backend to create REST API.'
     
     const developer2='I am nayem hasan. I have completed my BSc. in computer science and engineering. Now i am a full stack web developer. In this project, My role is Frontend Developer.';
 
