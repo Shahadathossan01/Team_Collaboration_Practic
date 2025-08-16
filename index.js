e12ddc3cd4a276918244db4e36680e5ba85ef526
topu <topu@gmail.com>
2025-08-16 15:43:48 +0600

    Second commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const TeamDetails=()=>{
+    //todo developer-1 -> Create a variabe named: developer1 and write your self;
+    
+    //todo developer-2 -> Create a variabe named: developer1 and write your self;
+
+    //todo developer-3 -> Write a description aboove tow developer; Here use stash.... 
+
+    //todo developer-4 -> console.log result;
+
+}
+
+TeamDetails()
